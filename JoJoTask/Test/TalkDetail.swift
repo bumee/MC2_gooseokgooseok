d96b09ab577d26178814ab0ee6ff77c228db1550
CODEMASTERJO <107622687+CODEMASTERJO@users.noreply.github.com>
2023-05-03 22:26:59 +0900

    JoJo 2023/05/03 - 질문답변화면구현

--- JoJoTask/Test/TalkDetail.swift
+++ JoJoTask/Test/TalkDetail.swift
@@ -9,9 +9,45 @@ import SwiftUI
 
 struct TalkDetail: View {
     var body: some View {
-        HStack {
+        VStack(spacing: 0) {
+            Spacer()
+            ZStack {
+                drawRectangle()
+                VStack {
+                    Text("04/21")
+                    Text("가장 소중하게 여기는 우리 가족의 추억은 뭔가요?")
+                        .padding(.bottom, 16)
+                    
+                    VStack {
+                        Text("답변자")
+                            .padding(.leading, 250)
+                        Text("🧑👩🏼‍🦱")
+                            .padding(.leading, 250)
+                    }
+                }
+                
+            }
             
+            Form {
+                HStack {
+                    Text("질문을 입력하세요")
+                    Spacer()
+                    Button("Send") {
+                        
+                    }
+                }
+                
+                
+            }
+            .background(Color.white)
+            .padding(.top, 250)
         }
+    }
+    
+    func drawRectangle() -> some View {
+        RoundedRectangle(cornerRadius: 8)
+            .strokeBorder(.black, lineWidth: 2)
+            .padding(10)
     }
 }
 
