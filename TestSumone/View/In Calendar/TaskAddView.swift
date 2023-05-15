09efc03a83e6f715df926e55e131d14733e4f59d
Moon <59304977+mun9769@users.noreply.github.com>
2023-05-15 23:07:55 +0900

    Eren 2023/05/15 - TaskAddView fix
    수정인지 아닌지 판별하는 변수 선언함.
    uuidStr을 찾아서 수정할 수 있도록 해줌

--- TestSumone/View/In Calendar/TaskAddView.swift
+++ TestSumone/View/In Calendar/TaskAddView.swift
@@ -10,36 +10,43 @@ import SwiftUI
 
 struct TaskAddView: View {
     
+    @State var uuidStr: String?
     @State var anniName: String = ""
     @State var anniDate: Date = Date()
+    @State var isEditting: Bool = false
     @Binding var showModal : Bool
     
-    var myTask: TaskManager
+    var myTask: TaskManager = calendarManager
     
     var body: some View {
         NavigationView() {
-            List {
-                Section() {
-                    DatePicker("날짜", selection: $anniDate, displayedComponents: [.date] )
-                        .environment(\.locale, Locale(identifier: "ko_KR"))
-                        .datePickerStyle(.compact)
-                }
-                Section(){
-                    TextField("제목", text: $anniName)
+            VStack{
+                List {
+                    Section() {
+                        DatePicker("날짜", selection: $anniDate, displayedComponents: [.date] )
+                            .environment(\.locale, Locale(identifier: "ko_KR"))
+                            .datePickerStyle(.compact)
+                    }
+                    Section(){
+                        TextField("제목", text: $anniName)
+                    }
                 }
             }
-            
             .navigationTitle("새로운 기념일")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .confirmationAction) {
                     Button(action: {
-                        myTask.addTask(self.anniDate, self.anniName)
+                        if !isEditting{
+                            myTask.addTask(self.anniDate, self.anniName)
+                        } else {
+                            myTask.updateTask(self.uuidStr!, self.anniDate, self.anniName)
+                        }
                         
                         self.showModal = false
                         
                     }, label: {
-                        Text("완료")
+                        Text(isEditting ? "수정" : "완료")
                     })
                     .disabled(self.anniName == "" ? true : false)
                 }
@@ -55,4 +62,10 @@ struct TaskAddView: View {
             }
         }
     }
+}
+
+struct TaskAddView_Previews: PreviewProvider {
+  static var previews: some View {
+      TaskAddView(showModal: .constant(false), myTask: TaskManager())
+  }
 }
